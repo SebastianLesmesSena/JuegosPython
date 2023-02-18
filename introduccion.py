c56dabc5a922918d23fbe6f834acd1c62b79bd17
lesmesSena <juan.martinez124@misena.edu.co>
2023-02-18 12:18:06 -0500

    creo repositorio

---
+++ introduccion.py
@@ -0,0 +1,9 @@
+nombre =input("Coloca tu nombre: ")
+print("Hola ",nombre)
+
+num1= int(input("ingrese el primer numero: "))
+num2= int(input("ingrese el segundo numero: "))
+
+total=(num1+num2)
+
+print("La suma entre ",num1," y ",num2," da: ",total)
